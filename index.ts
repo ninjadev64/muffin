bb3237cc8caa6def6270b9c139c99588b9b4fb2b
ninjadev64 <63245705+ninjadev64@users.noreply.github.com>
2024-07-07 16:21:40 +0100

    Use Deno formatter and linter

--- index.ts
+++ index.ts
@@ -3,7 +3,7 @@ import { parse, stringify } from "npm:himalaya";
 // Configuration options
 let port = 5200;
 let scheme = "http";
-for (let [ index, arg ] of Deno.args.entries()) {
+for (let [index, arg] of Deno.args.entries()) {
 	arg = arg.trim().toLowerCase();
 	if (arg == "--port" || arg == "-p") {
 		port = parseInt(Deno.args[index + 1] ?? "5200");
@@ -23,12 +23,12 @@ const desiredReqHeaders = [
 	"cookie",
 	"sec-ch-ua",
 	"sec-ch-ua-mobile",
-	"sec-ch-ua-platform"
+	"sec-ch-ua-platform",
 ];
 
 // A list of headers received from the client that should be forwarded to the proxied server.
 const desiredResHeaders = [
-	"content-type"
+	"content-type",
 ];
 
 // CSS to apply to Muffin configuration and error pages.
@@ -56,15 +56,17 @@ input, button {
  * and prefix all src, srcset, href, and action attributes
  * with the hostname of the Muffin proxy.
  * @param {object} elements - The JSON representation of the HTML document.
-*/
+ */
 function recurse(elements: any, host: string | null): any {
-	for (const [ index, element ] of elements.entries()) {
-		let attributes: { key: string, value: string }[] = [];
+	for (const [index, element] of elements.entries()) {
+		let attributes: { key: string; value: string }[] = [];
 		for (let { key, value } of element.attributes ?? []) {
 			key = key.trim().toLowerCase();
 			if (value) {
 				if (key == "src" || key == "href" || key == "action") {
-					if (value.startsWith("http:") || value.startsWith("https:")) {
+					if (
+						value.startsWith("http:") || value.startsWith("https:")
+					) {
 						value = `/muffin_proxy/${value}`;
 					} else if (value.startsWith("//")) {
 						value = `/muffin_proxy/http:${value}`;
@@ -73,9 +75,12 @@ function recurse(elements: any, host: string | null): any {
 					}
 				} else if (key == "srcset") {
 					let sections = value.split(",");
-					for (let [ index, section ] of sections.entries()) {
+					for (let [index, section] of sections.entries()) {
 						section = section.trim();
-						if (section.startsWith("http:") || value.startsWith("https:")) {
+						if (
+							section.startsWith("http:") ||
+							value.startsWith("https:")
+						) {
 							sections[index] = `/muffin_proxy/${section}`;
 						} else if (section.startsWith("//")) {
 							sections[index] = `/muffin_proxy/http:${section}`;
@@ -116,7 +121,7 @@ function generateErrorMessage(e: Error): string {
  * @param {Request} req
  */
 async function requestHandler(req: Request): Promise<Response> {
-	let host: { host: string, differentToConfigured: boolean };
+	let host: { host: string; differentToConfigured: boolean };
 	// Check if the user is accessing the muffin_init route or the muffin_proxy route (in order to use an host other than the configured one).
 	let url = new URL(req.url).pathname;
 	if (url.startsWith("/muffin_init")) {
@@ -139,20 +144,28 @@ async function requestHandler(req: Request): Promise<Response> {
 					</body>
 				</html>
 			`;
-			return new Response(body, { status: 200, headers: { "content-type": "text/html" } });
+			return new Response(body, {
+				status: 200,
+				headers: { "content-type": "text/html" },
+			});
 		} else {
 			// Make a request to the host in order to determine if it redirects to another host.
 			let res = await fetch(`http://${domain}`);
 			if (res.redirected) domain = new URL(res.url).host ?? domain;
 			let headers = {
 				"location": `/`,
-				"set-cookie": `muffin_host=${domain}; Path=/; HttpOnly; SameSite=Lax`
+				"set-cookie": `muffin_host=${domain}; Path=/; HttpOnly; SameSite=Lax`,
 			};
 			return new Response(null, { status: 302, headers });
 		}
 	} else if (url.startsWith("/muffin_proxy")) {
 		let u = new URL(url.slice(14));
-		if (!u) return new Response(generateErrorMessage(new Error("Invalid URL")), { status: 400 });
+		if (!u) {
+			return new Response(
+				generateErrorMessage(new Error("Invalid URL")),
+				{ status: 400 },
+			);
+		}
 		let res = await fetch(`http://${u.host}`);
 		if (res.redirected) u.host = new URL(res.url).host;
 		host = { host: u.host, differentToConfigured: true };
@@ -160,12 +173,21 @@ async function requestHandler(req: Request): Promise<Response> {
 	} else {
 		// If accessing a path relative to the configured host, parse the cookie header to obtain the configured host.
 		let cookies = req.headers.get("cookie") ?? "muffin_host=";
-		const { muffin_host } = cookies.split(";").map((cookie) => cookie.split("=")).reduce((obj: any, pair) => {
-			obj[decodeURIComponent(pair[0].trim())] = decodeURIComponent(pair[1].trim());
+		const { muffin_host } = cookies.split(";").map((cookie) =>
+			cookie.split("=")
+		).reduce((obj: any, pair) => {
+			obj[decodeURIComponent(pair[0].trim())] = decodeURIComponent(
+				pair[1].trim(),
+			);
 			return obj;
 		}, {});
 		if (!muffin_host) {
-			return new Response(null, { status: 302, headers: { "location": `${scheme}://${req.headers.get("host")}/muffin_init` } });
+			return new Response(null, {
+				status: 302,
+				headers: {
+					"location": `${scheme}://${req.headers.get("host")}/muffin_init`,
+				},
+			});
 		}
 		host = { host: muffin_host, differentToConfigured: false };
 	}
@@ -175,21 +197,32 @@ async function requestHandler(req: Request): Promise<Response> {
 		// Copy the headers to be forwarded to the proxied server.
 		let reqHeaders: any = {};
 		for (const header of desiredReqHeaders) {
-			if (req.headers.has(header)) reqHeaders[header] = req.headers.get(header);
+			if (req.headers.has(header)) {
+				reqHeaders[header] = req.headers.get(header);
+			}
 		}
 		// Request the proxied server with the appropriate parameters.
-		dat = await fetch(`http://${host.host}${url}`, { method: req.method, headers: reqHeaders, body: [ "GET", "HEAD" ].includes(req.method) ? undefined : await req.blob() });
+		dat = await fetch(`http://${host.host}${url}`, {
+			method: req.method,
+			headers: reqHeaders,
+			body: ["GET", "HEAD"].includes(req.method) ? undefined : await req.blob(),
+		});
 	} catch (e) {
-		return new Response(generateErrorMessage(e), { status: 400, headers: { "content-type": "text/html" } });
+		return new Response(generateErrorMessage(e), {
+			status: 400,
+			headers: { "content-type": "text/html" },
+		});
 	}
 
 	// Copy the headers to be forwarded to the client.
 	let resHeaders: any = {};
 	for (const header of desiredResHeaders) {
-		if (dat.headers.has(header)) resHeaders[header] = dat.headers.get(header);
+		if (dat.headers.has(header)) {
+			resHeaders[header] = dat.headers.get(header);
+		}
 	}
 
-	if ([ 101, 204, 205, 304 ].includes(dat.status)) {
+	if ([101, 204, 205, 304].includes(dat.status)) {
 		return new Response(null, { status: dat.status, headers: resHeaders });
 	}
 
@@ -199,15 +232,25 @@ async function requestHandler(req: Request): Promise<Response> {
 	let tbw;
 	if (contentType && contentType.startsWith("text/html")) {
 		try {
-			tbw = stringify(recurse(parse(await dat.clone().text()), host.differentToConfigured ? host.host : null));
+			tbw = stringify(
+				recurse(
+					parse(await dat.clone().text()),
+					host.differentToConfigured ? host.host : null,
+				),
+			);
 		} catch {
-			tbw = "Muffins are undercooked (failed to parse the requested HTML document, raw contents returned)" + await dat.clone().text();
+			tbw =
+				"Muffins are undercooked (failed to parse the requested HTML document, raw contents returned)" +
+				await dat.clone().text();
 		}
 	}
 	if (!tbw) tbw = new Uint8Array(await dat.arrayBuffer());
 
 	const resultUrl = new URL(dat.url);
-	if (dat.redirected && (resultUrl.host != host.host || resultUrl.pathname != url)) {
+	if (
+		dat.redirected &&
+		(resultUrl.host != host.host || resultUrl.pathname != url)
+	) {
 		if (host.differentToConfigured || resultUrl.host != host.host) {
 			resHeaders["location"] = `/muffin_proxy/${dat.url}`;
 		} else {
@@ -224,7 +267,9 @@ Deno.serve({ port }, async (req) => {
 	try {
 		return await requestHandler(req);
 	} catch (e) {
-		let message = `Muffin batter was spilt while adding ${URL.parse(req.url)?.pathname}: ${e.message}`;
+		let message = `Muffin batter was spilt while adding ${
+			URL.parse(req.url)?.pathname
+		}: ${e.message}`;
 		console.error(message);
 		return new Response(message);
 	}
